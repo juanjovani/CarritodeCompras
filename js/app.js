605b12db7ef634e3ca19d7b7f025f70b03132dda
juanjovani <juandegante34@outlook.com>
2022-10-03 11:08:32 -0500

    Recuperamos de un objeto seleccionado

--- js/app.js
+++ js/app.js
@@ -4,13 +4,28 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
+let ListadoCarrido = [];
 
 const agregarCurso= (e) =>{
     e.preventDefault();
-    console.log(e.target.classList);
     if(e.target.classList.contains('agregar-carrito')){
-        console.log("presionaste el boton");
+        const curso=e.target.parentElement;
+        const infoCurso ={
+            imagen:curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+            cantidad: 1
+        }
+
+        agregarCarrito(infoCurso);        
     }
+    
+    
+}
+const agregarCarrito= curso =>{
+    ListadoCarrido =[...ListadoCarrido, curso]
+    console.log(ListadoCarrido);
 }
 
 const cargarEventListener = () =>{
