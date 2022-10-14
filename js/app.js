559f941c37fa4e443a5237527f5226f210f9f93d
juanjovani <juandegante34@outlook.com>
2022-10-14 12:06:03 -0500

    tercer_commit

--- js/app.js
+++ js/app.js
@@ -1,61 +1,92 @@
-//variables para objetos del documentos
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
-let ListadoCarrido = [];
+let listadoCarrito = [];
 
-const agregarCurso= (e) =>{
+const agregarCurso = (e) => {
     e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const curso=e.target.parentElement.parentElement;
-        const infoCurso ={
-            imagen:curso.querySelector('img').src,
-            nombre: curso.querySelector('h4').textContent,
-            precio: curso.querySelector('p.precio').textContent,
-            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
-            cantidad: 1
+    if(e.target.classList.contains('agregar-carrito')) {
+        const curso = e.target.parentElement.parentElement;
+        const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        nombre: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('p.precio').textContent,
+        id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+        cantidad : 1
         }
+        agregarCarrito(infoCurso);
 
-        agregarCarrito(infoCurso);        
     }
-    
-    
-}
-const agregarCarrito= curso =>{
-    ListadoCarrido =[...ListadoCarrido, curso]
-    console.log(ListadoCarrido);
-    generaHTML();
 
 }
+    const agregarCarrito = curso => {
+       // console.log("Curso a agregar");
+        //console.log(curso.id)
+        //console.log("Listado de cursos");
+        //listadoCarrito.forEach (curso => console.log(curso.id) );
+        if(listadoCarrito.some(cursoInCarrito => cursoInCarrito.id === curso.id)) {
+        let carrito = listadoCarrito.map(cursoInCarrito => {
+            if(cursoInCarrito.id == curso.id) {
+                cursoInCarrito.cantidad++;
+                return cursoInCarrito;
+            }else{
+                return cursoInCarrito;
+            }
+        })
+            listadoCarrito = [...carrito];
+        } else {
+            listadoCarrito = [...listadoCarrito, curso];
+        }
+        console.log(listadoCarrito);
+        generHTML();
+    }
+    const generHTML = () => {
+        vaciarCarrito();
+        listadoCarrito.forEach (curso => {
+            const row = document.createElement('tr');
+            const cursoHTML = ` 
+            <td>
+            <img src="${curso.imagen}" width= 100>
+            </td>
 
-const generaHTML = () =>{
-    vaciarCarrito();
-    ListadoCarrido.forEach(curso =>{
-        const row= document.createElement('tr');
-        const cursoHTML = `
-        <td><img src=" ${curso.imagen}" width=100></td>
-    <td>${curso.nombre}</td>
-    <td>${curso.precio}</td>
+            <td>${curso.nombre} </td>
+        <td>${curso.precio}</td>
     <td>${curso.cantidad}</td>
-    
-    `;
-    row.innerHTML = cursoHTML;
-    contenedorCarrito.appendChild(row);
+    <td>
+           <a href = "#" class ="borrar-curso" data id="$(curso.id)">x</a
+           </td>
+
+            `;
+            row.innerHTML = cursoHTML;
+            contenedorCarrito.appendChild(row);
 
     });
 }
 
-const vaciarCarrito= () =>{
+    const vaciarCarrito = () => {
+        contenedorCarrito.innerHTML = ""; 
+    }
 
+    const eliminarCurso = (e) => {
+        e.preventDefault();
+        if(e.target.classList.contains('borrar-curso')) {
+            let idCurso = e.target.getAttribute('data-id')
+            let carrito = listadoCarrito.filter(cursoInCarrito => cursoInCarrito.id !== idCurso)
+        listadoCarrito = [...carrito];
+        generHTML();
+    }
 }
+    
+    
 
 
+const cargarEventListener = () => {
+    //Agregar funcion de carga de cursos al carrito
 
-const cargarEventListener = () =>{
-    //agregar funcion de carga de cursos al carrito
-    listaCursos.addEventListener('click',agregarCurso);
-
+    listaCursos.addEventListener('click', agregarCurso);
+    contenedorCarrito.addEventListener('click', eliminarCurso);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 }
 cargarEventListener();
